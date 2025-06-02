85ca3ae37b895fd6593599ee1b175261f6d1b30b
Thapakorn Pipatpajong <thapakorn.ppj@gmail.com>
2025-06-02 20:12:03 +0700

    add maxsim and best search function for ColBERT

--- naive.py
+++ naive.py
@@ -5,8 +5,10 @@ from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-def frame_sampling(video_name : str, freq : int = 60) -> int:
+def frame_sampling(video_name: str, freq: int = 60) -> int:
     cap = cv2.VideoCapture(f"./example_video/{video_name}.mp4")
     if not os.path.exists("sampled_frames"):
         os.makedirs("sampled_frames")
@@ -25,24 +27,51 @@ def frame_sampling(video_name : str, freq : int = 60) -> int:
     cv2.destroyAllWindows()
     return current_frame + 1
 
-def video_patch_embedding(filename : str, title : str, total_frames : int) -> torch.Tensor:
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
+def video_patch_embedding(filename: str, title: str, total_frames: int) -> torch.Tensor:
     embedded = []
     for i in range(total_frames):    
-        # desc = f"Scene {i+1} from {title}."
         image = Image.open(f"./sampled_frames/{filename}_{i}.jpg")
+        desc = f"Scene {i} from {title}"
 
-        # inputs = processor(text=desc, images=image, return_tensors="pt", padding=True)
-        inputs = processor(images=image, return_tensors="pt", padding=True).to(device)
+        inputs = clip_processor(text=desc, images=image, return_tensors="pt", padding=True).to(device)
         with torch.no_grad():
-            outputs = model.vision_model(**inputs)
-        tokens = outputs.last_hidden_state.squeeze(0)
-        print(tokens)
-        print(torch.nn.functional.normalize(tokens, dim=-1))
-        embedded.append(torch.nn.functional.normalize(tokens, dim=-1))
+            outputs = clip_model(**inputs)
+        tokens = outputs.vision_model_output.last_hidden_state.squeeze(0)
+        visual_proj = clip_model.visual_projection(tokens)
+        embedded.append(torch.nn.functional.normalize(visual_proj, dim=-1))
     return torch.cat(embedded, dim=0)
 
+
+def text_embedding(text: str) -> torch.Tensor:
+    inputs = clip_processor(text=text, return_tensors="pt", padding=True).to(device)
+    with torch.no_grad():
+        outputs = clip_model.text_model(**inputs)
+    tokens = outputs.last_hidden_state.squeeze(0)    
+    return torch.nn.functional.normalize(tokens, dim=-1)
+
+
+def MaxSim(Eq: torch.Tensor, Ed: torch.Tensor) -> int:
+    scores = torch.matmul(Eq, Ed.T)
+    max_row = scores.max(dim=1).values
+    return max_row.sum()
+
+
+def best_match(Eq: torch.Tensor, list_Ed: list) -> tuple[torch.Tensor, int]:
+    ret_vec = None
+    ret_score = float("-inf")
+    for Ed in list_Ed:
+        tmp_score = MaxSim(Eq, Ed)
+        if tmp_score > ret_score:
+            ret_score = tmp_score
+            ret_vec = Ed
+    return ret_vec, ret_score
+
+
 if __name__ == "__main__":
-    video_patch_embedding("bigbang", "the big bang theory TV show", 3)
+    document = []
+    doc = video_patch_embedding("bigbang", "the big bang theory TV show", 3)
+    document.append(doc)
+    query_embedded = text_embedding("Describe the video about the scene in the big bang theory TV show")
+    print(best_match(query_embedded, document))
+    
